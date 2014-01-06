56857723993ea7dd682464d7ec86324bfcd7adef
Simon <simon.mahe@siclic.fr>
2014-01-06 14:40:45 +0100

    Ajout des librairies Fallback en locale

--- javascript/startup.js
+++ javascript/startup.js
@@ -11,25 +11,27 @@ requirejs.config({
 		*/
 		'text'               : '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.10/text.min',
 
-		'jquery'             : '//code.jquery.com/jquery-2.0.3.min',
-		'bootstrap'          : '//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min',
-		'underscore'         : '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min',
-		'underscore.string'  : '//cdnjs.cloudflare.com/ajax/libs/underscore.string/2.3.3/underscore.string.min',
-		'moment'             : '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.4.0/moment-with-langs.min',
-		'jquery.maskedinput' : '//cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.3.1/jquery.maskedinput.min',
+		'jquery'             : ['//code.jquery.com/jquery-2.0.3.min', 'libs/jquery-2.0.3.min'],
+		'bootstrap'          : ['//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min', 'libs/bootstrap.3.0.3.min'],
+		'underscore'         : ['//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min', 'libs/underscore.1.5.2.min'],
+		'underscore.string'  : ['//cdnjs.cloudflare.com/ajax/libs/underscore.string/2.3.3/underscore.string.min', 'libs/underscore.string.2.3.3.min'],
+		'moment'             : ['//cdnjs.cloudflare.com/ajax/libs/moment.js/2.5.0/moment-with-langs.min', 'libs/moment.2.5.0.min'],
+		'jquery.maskedinput' : ['//cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.3.1/jquery.maskedinput.min', 'libs/jquery.maskedinput.1.3.1.min'],
 		
-		'select2'            : '//cdnjs.cloudflare.com/ajax/libs/select2/3.4.4/select2.min',
-		'select2-lang'       : '//cdnjs.cloudflare.com/ajax/libs/select2/3.4.4/select2_locale_fr.min',
-		'datepicker'         : '//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.2.0/js/bootstrap-datepicker.min',
-		'datepicker-lang'    : '//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.2.0/js/locales/bootstrap-datepicker.fr.min',
+		'select2'            : ['//cdnjs.cloudflare.com/ajax/libs/select2/3.4.4/select2.min', 'libs/select2.3.4.5.min'],
+		'select2-lang'       : ['//cdnjs.cloudflare.com/ajax/libs/select2/3.4.5/select2_locale_fr.min', 'libs/select2.3.4.5.locale.fr.min'],
+		'datepicker'         : ['//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.2.0/js/bootstrap-datepicker.min', 'libs/bootstrap-datepicker.1.2.0.min'],
+		'datepicker-lang'    : ['//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.2.0/js/locales/bootstrap-datepicker.fr.min', 'libs/bootstrap-datepicker.1.2.0.locale.fr.min'],
 		'timepicker'         : 'libs/timepicker-0.2.6',
 
 
+		/* ########
+		* JS
+		*/
 		'app'                : 'app',
 		'main'               : 'main',
-		'helper'             : 'helper',
+		'helper'             : 'Helper',
 		'advanceSelectBox'   : 'AdvanceSelectBox',
-
 		'bookingsModel'      : 'BookingsModel',
 
 
