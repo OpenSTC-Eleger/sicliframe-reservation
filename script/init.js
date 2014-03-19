f9dfbf6497ae9cfb8c4569bae4c41e8b34454cbf
Simon <simon.mahe@siclic.fr>
2014-03-19 15:15:03 +0100

    Add banner license on all script and style file

--- script/init.js
+++ script/init.js
@@ -1,3 +1,21 @@
+/*! 
+ * Sicliframe-resa
+ * Copyright 2013-2014 Siclic <contact@siclic.fr>
+ * Licensed under AGPL-3.0 (https://www.gnu.org/licenses/agpl.txt)
+ */
+
+/*! 
+ * Sicliframe-resa
+ * Copyright 2013-2014 Siclic <contact@siclic.fr>
+ * Licensed under AGPL-3.0 (https://www.gnu.org/licenses/agpl.txt)
+ */
+
+/*! 
+ * Sicliframe-resa
+ * Copyright 2013-2014 Siclic <contact@siclic.fr>
+ * Licensed under AGPL-3.0 (https://www.gnu.org/licenses/agpl.txt)
+ */
+
 require([
 	'main',
 ], function(main) {
