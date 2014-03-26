c75a36ca07afab795a1c93182fbd0b945da6de90
Simon <simon.mahe@siclic.fr>
2014-03-26 16:13:36 +0100

    Update select2 lib to 3.4.6

--- script/startup.js
+++ script/startup.js
@@ -24,8 +24,8 @@ requirejs.config({
 		'moment'             : 'libs/moment.2.5.0.min',
 		'jquery.maskedinput' : 'libs/jquery.maskedinput.1.3.1.min',
 
-		'select2'            : 'libs/select2.3.4.5.min',
-		'select2-lang'       : 'libs/select2.3.4.5.locale.fr.min',
+		'select2'            : 'libs/select2.3.4.6.min',
+		'select2-lang'       : 'libs/select2.3.4.6.locale.fr.min',
 		'datepicker'         : 'libs/bootstrap-datepicker.1.3.0.min',
 		'datepicker-lang'    : 'libs/bootstrap-datepicker.1.3.0.locale.fr.min',
 		'timepicker'         : 'libs/timepicker-0.2.6',
